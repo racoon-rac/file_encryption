74c9dc015db0c90277fb0d93cbb47ed32ba6dc3e
racoon-rac <64579294+racoon-rac@users.noreply.github.com>
2020-05-07 17:17:46 +0900

    Create addtool.h

---
+++ addtool.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <bitset>
+
+using namespace std;
+
+
+void filein( const char* r1_plachrv, vector<long long>& r1_plav);
+
+void filewrite(const char* r5_filename, vector<long long>& r5_encv);
